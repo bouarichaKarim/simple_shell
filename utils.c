0694b1ee7bf4d98111190ef753222a739e89b4b5
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-24 00:40:55 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,29 +1,54 @@
 #include "shell.h"
 
 /**
- * execute_command - execute command, and handles both
- * regular commands and comments.
- * @command: param command line.
+ * execute_exit_command - execute the "exit" command.
+ * @command: command line.
  * Return: no return.
  */
+void execute_exit_command(char *command)
+{
+	char *status_arg = command + 4;
 
+	while (*status_arg != '\0')
+	{
+		if (!isspace(*status_arg))
+		{
+			int status = atoi(status_arg);
+
+			exit(status);
+		}
+		status_arg++;
+	}
+	exit(0);
+}
+
+/**
+ * execute_shell_command - execute the shell command.
+ * @command: command line.
+ * Return: no return.
+ */
+void execute_shell_command(char *command)
+{
+	char *argv[] = {"/bin/sh", "-c", NULL, NULL};
+
+	argv[2] = command;
+	if (execve(argv[0], argv, NULL) == -1)
+	{
+		perror("Error executing command");
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * execute_command - execute command and handle both regular commands and comments.
+ * @command: command line.
+ * Return: no return.
+ */
 void execute_command(char *command)
 {
 	if (strncmp(command, "exit", 4) == 0)
 	{
-		char *status_arg = command + 4;
-
-		while (*status_arg != '\0')
-		{
-			if (!isspace(*status_arg))
-			{
-				int status = atoi(status_arg);
-
-				exit(status);
-			}
-			status_arg++;
-		}
-		exit(0);
+		execute_exit_command(command);
 	}
 	else
 	{
@@ -38,14 +63,7 @@ void execute_command(char *command)
 		}
 		else if (pid == 0)
 		{
-			char *argv[] = {"/bin/sh", "-c", NULL, NULL};
-
-			argv[2] = command;
-			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("Error executing command");
-				exit(EXIT_FAILURE);
-			}
+			execute_shell_command(command);
 		}
 		else
 		{
