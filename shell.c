01bdaa0f48735bc119b5fa578665b8346d98bcae
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-23 16:25:32 -0700

    betty fix

--- shell.c
+++ shell.c
@@ -42,50 +42,50 @@ int main(void)
 void execute_command(char *command)
 {
 	if (strncmp(command, "exit", 4) == 0)
-    {
-        char *status_arg = command + 4;
-		
-        while (*status_arg != '\0')
-        {
-            if (!isspace(*status_arg))
-            {
-                int status = atoi(status_arg);
-		    
-                exit(status);
-            }
-            status_arg++;
-        }
-        exit(0);
-    }
-    else
-    {
-        pid_t pid;
-        int status;
+	{
+		char *status_arg = command + 4;
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("Error forking");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            char *argv[] = {"/bin/sh", "-c", NULL, NULL};
+		while (*status_arg != '\0')
+		{
+			if (!isspace(*status_arg))
+			{
+				int status = atoi(status_arg);
 
-            argv[2] = command;
-            if (execve(argv[0], argv, NULL) == -1)
-            {
-                perror("Error executing command");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
-            if (status != 0)
-            {
-                printf("%s: command not found\n", command);
-            }
-        }
-    }
+				exit(status);
+			}
+			status_arg++;
+		}
+		exit(0);
+	}
+	else
+	{
+		pid_t pid;
+		int status;
+
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Error forking");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			char *argv[] = {"/bin/sh", "-c", NULL, NULL};
+
+			argv[2] = command;
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error executing command");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+			if (status != 0)
+			{
+				printf("%s: command not found\n", command);
+			}
+		}
+	}
 }
