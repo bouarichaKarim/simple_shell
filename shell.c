333acd533a159fcd5c406b21eff13010230ee5e2
MTH35H4RK <switchayman@gmail.com>
2023-05-23 19:12:43 -0700

    exit working again

--- shell.c
+++ shell.c
@@ -4,30 +4,33 @@
  * main - Entry point for the shell program.
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char *command = NULL;
-	size_t bufsize = 0;
-	ssize_t characters;
+        char *command = NULL;
+        size_t bufsize = 0;
+        ssize_t characters;
 
-	while (1)
-	{
-		prompt();
-		characters = getline(&command, &bufsize, stdin);
+        while (1)
+        {
+                prompt();
+                characters = getline(&command, &bufsize, stdin);
 
-		if (characters == -1)
-		{
-			printf("\n");
-			break;
-		}
+                if (characters == -1)
+                {
+                        printf("\n");
+                        break;
+                }
 
-		if (characters > 0 && command[characters - 1] == '\n')
-			command[characters - 1] = '\0';
+                if (characters > 0 && command[characters - 1] == '\n')
+                        command[characters - 1] = '\0';
 
-		execute_command(command);
-	}
+                /* Skip executing empty command */
+                if (command[0] == '\0')
+                        continue;
 
-	free(command);
-	return (0);
+                execute_command(command);
+        }
+
+        free(command);
+        return (0);
 }
--- utils.c
+++ utils.c
@@ -1,21 +1,10 @@
 #include "shell.h"
 
-void execute_exit_command(char *command)
-{
-    char *status_arg = command + 4;
-
-    while (*status_arg != '\0')
-    {
-        if (!isdigit(*status_arg))
-        {
-            int status = atoi(status_arg);
-            exit(status);
-        }
-        status_arg++;
-    }
-    exit(0);
-}
-
+/**
+ * execute_shell_command - execute the shell command.
+ * @command: command line.
+ * Return: no return.
+ */
 void execute_shell_command(char *command)
 {
     char *argv[] = {"/bin/sh", "-c", NULL, NULL};
@@ -28,62 +17,45 @@ void execute_shell_command(char *command)
     }
 }
 
-#include "shell.h"
-
-void execute_command(char *command) {
-    pid_t pid;
-    int status;
-    int pipefd[2];
-
-    char *path = get_command_path(command);
-    char **arguments = parse_arguments(command);
-
-    if (path == NULL) {
-        fprintf(stderr, "%s: command not found\n", get_program_name());
-        free_arguments(arguments);
-        return;
+/**
+ * execute_command - execute command and handle both
+ * regular commands and comments.
+ * @command: command line.
+ * Return: no return.
+ */
+void execute_command(char *command)
+{
+    if (strcmp(command, "exit") == 0)
+    {
+        exit(0);
     }
+    else
+    {
+        pid_t pid;
+        int status;
 
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        free_arguments(arguments);
-        return;
-    }
-
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        free_arguments(arguments);
-        return;
-    } else if (pid == 0) {
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-
-        if (execve(path, arguments, environ) == -1) {
-            perror(get_program_name());
-            free_arguments(arguments);
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("Error forking");
             exit(EXIT_FAILURE);
         }
-    } else {
-        close(pipefd[1]);
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
+        else if (pid == 0)
+        {
+            char *argv[] = {"/bin/sh", "-c", NULL, NULL};
 
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-            fprintf(stderr, "%s: %s: command not found\n", get_program_name(), command);
+            argv[2] = command;
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                char error_message[100];
+                snprintf(error_message, sizeof(error_message), "%s: 1: %s: not found\n", argv[0], argv[2]);
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            wait(&status);
         }
     }
-
-    free_arguments(arguments);
-    free(path);
-}
-
-char *get_program_name(void)
-{
-    extern char **environ;
-    char *program_path = environ[0];
-    char *program_name = strrchr(program_path, '/');
-    return (program_name != NULL) ? program_name + 1 : program_path;
 }
