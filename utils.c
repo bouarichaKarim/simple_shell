3d538d32660202627e5caeb20a831b7b1af41b66
MTH35H4RK <switchayman@gmail.com>
2023-05-24 14:30:13 -0700

    added the utils.c

---
+++ utils.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * execute_shell_command - execute a command of the shell
+ *
+ * @cmd: command string
+ * @env: pointer to pointer to an array of pointers to strings
+ * call of the environment
+ */
+void execute_shell_command(char *cmd, char **env)
+{
+        char **arg = NULL;
+        argument_exec *head = NULL;
+
+        strtok(cmd, "\n");
+        head = list_of_arg(cmd);
+        arg = val_of_arg(head);
+
+        if (execve(arg[0], arg, env) == -1)
+        {
+                argv_free(head);
+                free(arg);
+                perror("./hsh");
+                exit(EXIT_FAILURE);
+        }
+
+        argv_free(head);
+        free(arg);
+}
