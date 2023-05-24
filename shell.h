6b4b876b6dd6a6e987246fd90035f396695a9e30
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-24 15:04:48 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -4,18 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <ctype.h>
-#include <sys/stat.h>
-#include <sys/prctl.h>
-#include <sys/wait.h>
-
-#define INITIAL_SIZE 10
 
 extern char **environ;
-void execute_shell_command(char *command, char *program_name);
-void execute_command(char *command, char *program_name);
-void execute_builtin_command(char *command);
+
+void execute_command(char *command);
 void prompt(void);
 
 char **parse_arguments(char *line);
@@ -24,4 +20,7 @@ void free_arguments(char **arguments);
 char *get_command_path(char *command);
 char *create_command_path(char *command, char *directory);
 
-#endif /* SHELL_H */
+void execute_builtin_command(char *command);
+
+
+#endif
