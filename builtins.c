ba25d233062f1ff79fb938133df4e6f9e48a03af
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-24 15:01:52 +0100

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -6,40 +6,46 @@
  * @command: param command.
  * Return: no return.
  */
+
+
 void execute_builtin_command(char *command)
 {
-    if (strcmp(command, "exit") == 0)
-    {
-        exit(0);
-    }
-    else if (strncmp(command, "exit ", 5) == 0)
-    {
-        char *status_arg = command + 5;
-        int status;
-
-        while (*status_arg != '\0')
+	if (strcmp(command, "exit") == 0)
+	{
+		exit(0);
+	}
+	
+	else if (strncmp(command, "exit ", 5) == 0)
         {
-            if (!isdigit(*status_arg))
-            {
-                printf("Invalid status argument: %s\n", status_arg);
-                return;
-            }
-            status_arg++;
-        }
+                char *status_arg = command + 5;
+		int status;
 
-        status = atoi(command + 5);
-        exit(status);
-    }
-    else if (strcmp(command, "env") == 0)
-    {
-        int i = 0;
-        char *env_var = environ[i];
+                while (*status_arg != '\0')
+                {
+                        if (!isdigit(*status_arg))
+                        {
+                                printf("Invalid status argument: %s\n", status_arg);
+                                return;
+                        }
+                        status_arg++;
+                }
 
-        while (env_var != NULL)
-        {
-            printf("%s\n", env_var);
-            i++;
-            env_var = environ[i];
+                status = atoi(command + 5);
+                exit(status);
         }
-    }
+	
+	else if (strcmp(command, "env") == 0)
+	{
+		extern char **environ;
+
+                int i = 0;
+                char *env_var = environ[i];
+
+                while (env_var != NULL)
+                {
+                        printf("%s\n", env_var);
+                        i++;
+                        env_var = environ[i];
+                }
+	}
 }
