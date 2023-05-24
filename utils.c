b9ec5db200b34c69865fe4da661cc547eb3751b8
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-24 16:27:23 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -29,7 +29,8 @@ void execute_exit_command(char *command)
  */
 void execute_shell_command(char *command)
 {
-	char *argv[] = {"/bin/sh", "-c", NULL, NULL};
+char *argv[] = {"/bin/sh", "-c", NULL, NULL};
+argv[2] = command;
 
     pid_t pid = fork();
     if (pid == -1)
