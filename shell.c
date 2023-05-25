e2345d6ff172da195d36a74703f812788bebeb14
MTH35H4RK <switchayman@gmail.com>
2023-05-24 17:03:53 -0700

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,16 +1,13 @@
 #include "shell.h"
 
 /**
- * main - function that checks if our shell is called
+ * main - Entry point.
  *
  * Return: 0 on success
  */
 
 int main(void)
 {
-/*
- * check if this interactive or not
- */
 	if (isatty(STDIN_FILENO) == 1)
 	{
 		interactive();
