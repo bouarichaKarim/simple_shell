e6408876c94c64995d92cec2e186b98116f59dce
MTH35H4RK <switchayman@gmail.com>
2023-05-24 17:08:29 -0700

    Update exec_build

--- exec_build.c
+++ exec_build.c
@@ -25,20 +25,6 @@ int build_excute(char **arg)
 	}
 	for (; j < sizeof(builtin_functions) / sizeof(char *); j++)
 	{
-/*
- *  if there is a match execute the builtin command
- *
- *  The loop condition i < sizeof(builtin_func_list) / sizeof(char *)
- *  compares the value of i with the size of the builtin_func_list array
- *  divided by the size of a char* (the size of a pointer).
- *  sizeof(builtin_func_list) returns the total size of the builtin_func_list
- *  array in bytes.
- *
- *  sizeof(char *) returns the size of a char* pointer in bytes.
- *
- *  Dividing the total size of the array by the size of a pointer
- *  gives the number of elements in the array.
- **/
 		if (strcmp(arg[0], builtin_functions[j]) == 0)
 		{
 			return ((*builtin_func_code[j])(arg));
